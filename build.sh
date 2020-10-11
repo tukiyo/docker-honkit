d7e1266e28819e10efddc3f9f09a8768813c141f
tukiyo3 <tukiyo3@gmail.com>
2020-10-11 15:50:51 +0900

    github pages対応

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 set -eu
 
 docker run -it --rm -v $(pwd)/honkit:/srv/honkit tukiyo3/honkit honkit build
-sudo chown -R $USER honkit
+
+# ./_book/ が表示できるようにする。 (github pages でアンダースコアから始まるディレクトリが見えない問題対応)
+touch honkit/.nojekyll
+
+sudo chown -R $USER:$USER honkit
